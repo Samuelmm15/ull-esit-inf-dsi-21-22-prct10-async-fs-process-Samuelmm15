0829befa9c4f549174bf45394e9199cf5747f7e1
Samuel Martín Morales <72341631+Samuelmm15@users.noreply.github.com>
2022-04-26 17:29:42 +0100

    Fixed exercice 2

--- src/exercice2.ts
+++ src/exercice2.ts
@@ -1,28 +1,106 @@
 /* eslint-disable no-unused-vars */
 import {spawn} from 'child_process';
+import yargs from 'yargs';
+import chalk from 'chalk';
+import * as fs from 'fs';
 
 class CatGrepCommand {
-  constructor() {
-    this.run();
+  constructor(private fileRoute: string, private word: string, private method: number) {
+    if (this.method === 1) {
+      this.firstMethod();
+    } else if (this.method === 2) {
+      this.secondMethod();
+    } else {
+      console.log(chalk.red('The selected method does not exists'));
+    }
   }
-  private run() {
-    const catGrep = spawn('cat', ['src/Files/test.txt', '|', 'grep', 'Hola']);
-    catGrep.stdout.pipe(process.stdout); // Hay que tener en cuenta que el stdout sirve para leer el contenido del fichero, stdin, sirve para escribir
+  private firstMethod() { // Este es el método con el pipe
+    const cat = spawn('cat', [this.fileRoute]);
+    const grep = spawn('grep', [this.word]);
+    cat.stdout.pipe(grep.stdin);
+    let auxiliaryGrep = '';
+    let counter: number = 0;
+    grep.stdout.on('data', (piece) => {
+      auxiliaryGrep = piece.toString();
+    });
+    grep.on('close', () => {
+      console.log();
+      console.log(chalk.green('File Content:'));
+      console.log(chalk.grey(auxiliaryGrep));
+      const result = auxiliaryGrep.split(/\s+/);
+      result.forEach((item) => {
+        if (item === this.word) {
+          counter++;
+        }
+      });
+      if (counter === 0) {
+        console.log();
+        console.log(chalk.red(`There is no match of the filter ${this.word}`));
+      } else {
+        console.log();
+        console.log(chalk.blue(`The number of coincidencis is ${counter}`));
+      }
+    });
+  }
+  private secondMethod() { // Esto es sin el método pipe
+    const catGrep = spawn('cat', [this.fileRoute, 'grep', this.word]);
     let counter: number = 0;
     let catGrepAuxiliary = '';
     catGrep.stdout.on('data', (piece) => {
       catGrepAuxiliary = piece.toString();
     });
     catGrep.on('close', () => {
+      console.log();
+      console.log(chalk.green('File Content:'));
+      console.log(chalk.grey(catGrepAuxiliary));
       const result = catGrepAuxiliary.split(/\s+/);
       result.forEach((item) => {
-        if (item === 'Hola') {
+        if (item === this.word) {
           counter++;
         }
       });
-      console.log(`  The number of coincidencis is ${counter}`);
+      if (counter === 0) {
+        console.log();
+        console.log(chalk.red(`There is no match of the filter ${this.word}`));
+      } else {
+        console.log();
+        console.log(chalk.blue(`The number of coincidencis is ${counter}`));
+      }
     });
   }
 }
 
-const test = new CatGrepCommand();
+yargs.command({
+  command: 'catGrepOption',
+  describe: 'This command counts the number of coincidencis of a specific string in a specific file',
+  builder: {
+    file: {
+      describe: 'File name',
+      demandOption: true,
+      type: 'string',
+    },
+    word: {
+      describe: 'To comprobe the coincidences',
+      demandOption: true,
+      type: 'string',
+    },
+    method: {
+      describe: 'Selects the method that is gonna to use',
+      demandOption: true,
+      type: 'number',
+    },
+  },
+  handler(argv) {
+    if ((typeof argv.file === 'string') &&
+      (typeof argv.word === 'string') &&
+        (typeof argv.method === 'number')) {
+      if (fs.existsSync(argv.file) === true) {
+        const firstVersion = new CatGrepCommand(argv.file, argv.word, argv.method);
+      } else {
+        console.log(chalk.red('The file that is triying to use does not exist'));
+      }
+    }
+  },
+});
+
+yargs.parse();
