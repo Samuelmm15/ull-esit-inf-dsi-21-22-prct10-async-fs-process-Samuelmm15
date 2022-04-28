ac0e11845df7f00f11b4d2b812bb6ea412156117
Samuel Martín Morales <72341631+Samuelmm15@users.noreply.github.com>
2022-04-28 09:43:34 +0100

    Finished exercice 4

--- src/exercice4/exercice4.ts
+++ src/exercice4/exercice4.ts
@@ -4,7 +4,7 @@
 // Listar ficheros dentro de un directorio (Hecho)
 // Mostrar el contenido del fichero (similar a cat) (Hecho)
 // Borrar ficheros y directorios (HECHO)
-// Mover ficheros o directorios de un punto a, a un punto b
+// Mover ficheros o directorios de un punto a, a un punto b (NO FUNCIONA DE MANERA CORRECTA)
 // Hacer uso de yargs para controlar todo esto anterior
 
 import yargs from "yargs";
@@ -12,182 +12,193 @@ import chalk from 'chalk';
 import * as fs from 'fs';
 import inquirer from "inquirer";
 
-yargs.command({
-  command: 'file',
-  describe: 'Returns the type of a specific item of a route.',
-  builder: {
-  },
-  handler() {
-    if (process.argv.length !== 4) {
-      console.log(chalk.red('Please, specify a route to the item'));
-      console.log(chalk.yellow('The structure of the command is: node dist/exercice4/exercice4.js file [route]'));
-    } else {
-      if (typeof process.argv[3] === 'string') {
-        fs.lstat(process.argv[3], (err, stats) => {
-          if (err) {
-            console.log(chalk.red('The specified path does not exist'));
-          } else if (stats.isFile()) {
-            console.log(chalk.green('It is a File'));
-          } else if (stats.isDirectory()) {
-            console.log(chalk.green('It is a directory'));
-          }
-        });
-      }
-    }
-  },
-});
-
-yargs.command({
-  command: 'mkdir',
-  describe: 'Create a directory at the specific route.',
-  builder: {
-  },
-  handler() {
-    if (process.argv.length !== 4) {
-      console.log(chalk.red('Please, specify a route to create the item'));
-      console.log(chalk.yellow('The structure of the command is: node dist/exercice4/exercice4.js mkdir [route]'));
-    } else {
-      if (typeof process.argv[3] === 'string') {
-        fs.mkdir(process.argv[3], (err) => {
-          if (err) {
-            console.log(chalk.red('The specified path already exist.'));
-          } else {
-            console.log(chalk.green('The directory was succefully created'));
-          }
-        });
-      }
-    }
-  },
-});
-
-yargs.command({
-  command: 'ls',
-  describe: 'List the content of a specific path',
-  builder: {
-
-  },
-  handler() {
-    if (process.argv.length !== 4) {
-      console.log(chalk.red('Please, specify a route to list the items'));
-      console.log(chalk.yellow('The structure of the command is: node dist/exercice4/exercice4.js ls [route]'));
-    } else {
-      if (typeof process.argv[3] === 'string') {
-        fs.readdir(process.argv[3], (err, data) => {
-          if (err) {
-            console.log(chalk.red('The specified path is wrong'));
-          } else {
-            data.forEach((item) => {
-              console.log(chalk.cyan(item));
+export class CommandProgram {
+  constructor() {
+    this.run();
+  }
+  private run(): void {
+    yargs.command({
+      command: 'file',
+      describe: 'Returns the type of a specific item of a path.',
+      builder: {
+      },
+      handler() {
+        if (process.argv.length !== 4) {
+          console.log(chalk.red('Please, specify a path to the item'));
+          console.log(chalk.yellow('The structure of the command is: node dist/exercice4/exercice4.js file [path]'));
+        } else {
+          if (typeof process.argv[3] === 'string') {
+            fs.lstat(process.argv[3], (err, stats) => {
+              if (err) {
+                console.log(chalk.red('The specified path does not exist'));
+              } else if (stats.isFile()) {
+                console.log(chalk.green('It is a File'));
+              } else if (stats.isDirectory()) {
+                console.log(chalk.green('It is a directory'));
+              }
             });
           }
-        });
-      }
-    }
-  },
-});
+        }
+      },
+    });
 
-yargs.command({
-  command: 'cat',
-  describe: 'Show the content of a specified file.',
-  builder: {
-  },
-  handler() {
-    if (process.argv.length !== 4) {
-      console.log(chalk.red('Please, specify a route to show the content of a file.'));
-      console.log(chalk.yellow('The structure of the command is: node dist/exercice4/exercice4.js cat [route]'));
-    } else {
-      if (typeof process.argv[3] === 'string') {
-        fs.readFile(process.argv[3], (err, data) => {
-          if (err) {
-            console.log(chalk.red('The file specified does not exist.'));
-          } else {
-            console.log(data.toString());
+    yargs.command({
+      command: 'mkdir',
+      describe: 'Create a directory at the specific path.',
+      builder: {
+      },
+      handler() {
+        if (process.argv.length !== 4) {
+          console.log(chalk.red('Please, specify a path to create the item'));
+          console.log(chalk.yellow('The structure of the command is: node dist/exercice4/exercice4.js mkdir [path]'));
+        } else {
+          if (typeof process.argv[3] === 'string') {
+            fs.mkdir(process.argv[3], (err) => {
+              if (err) {
+                console.log(chalk.red('The specified path already exist.'));
+              } else {
+                console.log(chalk.green('The directory was succefully created'));
+              }
+            });
           }
-        });
-      }
-    }
-  },
-});
+        }
+      },
+    });
 
-yargs.command({
-  command: 'rm',
-  describe: 'Delete files and directorys.',
-  builder: {
-  },
-  handler() {
-    if (process.argv.length !== 4) {
-      console.log(chalk.red('Please, specify a route to delete a file or directory.'));
-      console.log(chalk.yellow('The structure of the command is: node dist/exercice4/exercice4.js rm [route]'));
-    } else {
-      if (typeof process.argv[3] === 'string') {
-        inquirer.prompt({type: "confirm", name: "Continue", message: `Are you sure to delete ${process.argv[3]} ?`})
-            .then((answers) => {
-              if (answers["Continue"] === true) {
-                fs.unlink(process.argv[3], (err) => {
-                  if (err) { // If the path is a directory
-                    fs.lstat(process.argv[3], (err, stats) => {
-                      if (err) {
-                        console.log(chalk.red('The specified path does not exist'));
-                      } else if (stats.isFile()) {
-                        console.log(chalk.green('There must be a problem to delete the File'));
-                      } else if (stats.isDirectory()) {
-                        fs.rmdir(process.argv[3], (err) => {
+    yargs.command({
+      command: 'ls',
+      describe: 'List the content of a specific path',
+      builder: {
+
+      },
+      handler() {
+        if (process.argv.length !== 4) {
+          console.log(chalk.red('Please, specify a route to list the items'));
+          console.log(chalk.yellow('The structure of the command is: node dist/exercice4/exercice4.js ls [path]'));
+        } else {
+          if (typeof process.argv[3] === 'string') {
+            fs.readdir(process.argv[3], (err, data) => {
+              if (err) {
+                console.log(chalk.red('The specified path is wrong'));
+              } else {
+                data.forEach((item) => {
+                  console.log(chalk.cyan(item));
+                });
+              }
+            });
+          }
+        }
+      },
+    });
+
+    yargs.command({
+      command: 'cat',
+      describe: 'Show the content of a specified file.',
+      builder: {
+      },
+      handler() {
+        if (process.argv.length !== 4) {
+          console.log(chalk.red('Please, specify a path to show the content of a file.'));
+          console.log(chalk.yellow('The structure of the command is: node dist/exercice4/exercice4.js cat [path]'));
+        } else {
+          if (typeof process.argv[3] === 'string') {
+            fs.readFile(process.argv[3], (err, data) => {
+              if (err) {
+                console.log(chalk.red('The file specified does not exist.'));
+              } else {
+                console.log(data.toString());
+              }
+            });
+          }
+        }
+      },
+    });
+
+    yargs.command({
+      command: 'rm',
+      describe: 'Delete files and directorys.',
+      builder: {
+      },
+      handler() {
+        if (process.argv.length !== 4) {
+          console.log(chalk.red('Please, specify a path to delete a file or directory.'));
+          console.log(chalk.yellow('The structure of the command is: node dist/exercice4/exercice4.js rm [path]'));
+        } else {
+          if (typeof process.argv[3] === 'string') {
+            inquirer.prompt({type: "confirm", name: "Continue", message: `Are you sure to delete ${process.argv[3]} ?`})
+                .then((answers) => {
+                  if (answers["Continue"] === true) {
+                    fs.unlink(process.argv[3], (err) => {
+                      if (err) { // If the path is a directory
+                        fs.lstat(process.argv[3], (err, stats) => {
                           if (err) {
-                            console.log(chalk.red('There must be a problem to delete the directory'));
-                          } else {
-                            console.log(chalk.green('The directory was succefully deleted'));
+                            console.log(chalk.red('The specified path does not exist'));
+                          } else if (stats.isFile()) {
+                            console.log(chalk.green('There must be a problem to delete the File'));
+                          } else if (stats.isDirectory()) {
+                            fs.rmdir(process.argv[3], (err) => {
+                              if (err) {
+                                console.log(chalk.red('There must be a problem to delete the directory'));
+                              } else {
+                                console.log(chalk.green('The directory was succefully deleted'));
+                              }
+                            });
                           }
                         });
+                      } else {
+                        console.log(chalk.green('The file was succefully deleted'));
                       }
                     });
                   } else {
-                    console.log(chalk.green('The file was succefully deleted'));
+                    console.log(chalk.red('The file or directory was not deleted'));
                   }
                 });
-              } else {
-                console.log(chalk.red('The file or directory was not deleted'));
-              }
-            });
-      }
-    }
-  },
-});
+          }
+        }
+      },
+    });
 
-yargs.command({ // COMPROBAR ESTE COMANDO, EXISTE UN PROBLEMA
-  command: 'cp',
-  describe: 'Copy and move files or directorys',
-  builder: {
-  },
-  handler() {
-    if (process.argv.length !== 5) {
-      console.log(chalk.red('Please, specify a route to obtain the file or directory and the destination.'));
-      console.log(chalk.yellow('The structure of the command is: node dist/exercice4/exercice4.js cp [Origin] [Destination]'));
-    } else {
-      if ((typeof process.argv[3] === 'string') && (typeof process.argv[4] === 'string')) {
-        fs.lstat(process.argv[3], (err, stats) => {
-          if (err) {
-            console.log(chalk.red('There must be a problem'));
-          } else if (stats.isFile()) {
-            fs.copyFile(process.argv[3], process.argv[4], (err) => {
+    yargs.command({ // COMPROBAR ESTE COMANDO, EXISTE UN PROBLEMA
+      command: 'cp',
+      describe: 'Copy and move files or directorys',
+      builder: {
+      },
+      handler() {
+        if (process.argv.length !== 5) {
+          console.log(chalk.red('Please, specify a path to obtain the file or directory and the destination.'));
+          console.log(chalk.yellow('The structure of the command is: node dist/exercice4/exercice4.js cp [Origin] [Destination]'));
+        } else {
+          if ((typeof process.argv[3] === 'string') && (typeof process.argv[4] === 'string')) {
+            fs.lstat(process.argv[3], (err, stats) => {
               if (err) {
-                console.log(chalk.red('There must be a problem to copy the File'));
-              } else {
-                console.log(chalk.green('The File was succefully copied'));
-              }
-            });
-          } else if (stats.isDirectory()) {
-            fs.cp(process.argv[3], process.argv[4], (err) => {
-              if (err) {
-                console.log(chalk.red('There must be a problem to copy the directory'));
-              } else {
-                console.log(chalk.green('The directory was succefully copied'));
+                console.log(chalk.red('There must be a problem'));
+              } else if (stats.isFile()) {
+                fs.copyFile(process.argv[3], process.argv[4], (err) => {
+                  if (err) {
+                    console.log(chalk.red('There must be a problem to copy the File'));
+                  } else {
+                    console.log(chalk.green('The File was succefully copied'));
+                  }
+                });
+              } else if (stats.isDirectory()) {
+                fs.cp(process.argv[3], process.argv[4], (err) => {
+                  if (err) {
+                    console.log(chalk.red('There must be a problem to copy the directory'));
+                  } else {
+                    console.log(chalk.green('The directory was succefully copied'));
+                  }
+                });
               }
             });
           }
-        });
-      }
-    }
-  },
-});
+        }
+      },
+    });
 
-yargs.parse();
+    yargs.parse();
+  }
+}
+
+// eslint-disable-next-line no-unused-vars
+const commandProgramObj = new CommandProgram();
+
--- tests/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import 'mocha';
-import {expect} from 'chai';
-import {add} from '../src/index';
-
-describe('Sonar Test', () => {
-  it('Add test', () => {
-    expect(add(1)).to.be.equal(1);
-  });
-});
