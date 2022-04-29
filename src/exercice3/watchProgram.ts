b6ea96849245828f9f0719098f00e0a1b1242719
Samuel Martín <alu0101359526@ull.edu.es>
2022-04-29 12:06:20 +0000

    Fixed exercice 3

--- src/exercice3/watchProgram.ts
+++ src/exercice3/watchProgram.ts
@@ -5,20 +5,38 @@
 // Contestar a las últimas preguntas
 
 import * as fs from 'fs';
+import chalk from 'chalk';
+import yargs from 'yargs';
 
-console.log('accede');
-fs.watch(`src/exercice3/notes/Samuel/newNote.json`, {persistent: true}, (eventType) => {
-  if (eventType === 'change') {
-    console.log('Modificado');
-  }
+yargs.command({
+  command: 'route',
+  describe: 'Specifies the path of the directory to watch',
+  builder: {
+  },
+  handler() {
+    if (process.argv.length !== 4) {
+      console.log(chalk.red('Enter the execution correctly'));
+      console.log(chalk.yellow('node dist/exercice3/watchProgram.js route [path]'));
+    } else {
+      fs.access(process.argv[3], fs.constants.F_OK, (err) => {
+        if (err) {
+          console.log(chalk.red('The specified path does not exist'));
+          console.log(chalk.green('Specified a new path'));
+        } else {
+          fs.watch(process.argv[3], {persistent: true}, (eventType, filename) => {
+            if (eventType === 'change') {
+              console.clear();
+              console.log(chalk.green(`Added a new File called ${filename}`));
+            } else if (eventType === 'rename') {
+              console.clear();
+              console.log(chalk.green(`Removed the File called ${filename}`));
+            }
+          });
+        }
+      });
+    }
+  },
 });
 
+yargs.parse();
 
-// watcher.addListener('change', (eventType, filename) => {
-//   console.log(eventType);
-//   console.log(filename);
-// });
-
-// watcher
-//     .on('add', (path) => console.log(`File ${path} has been added`))
-//     .on('addDir', (path) => console.log(`Directory ${path} has been added`));
